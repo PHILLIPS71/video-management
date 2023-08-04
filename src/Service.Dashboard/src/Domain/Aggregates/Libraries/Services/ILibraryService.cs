7ff99b50f3b105dc939ac959e047c3e9c43cd2d1
Jordan Phillips <jordypee27@gmail.com>
2023-08-04 16:27:04 +1000

    fix(dashboard): prevent non media files being scanned into a library

--- src/Service.Dashboard/src/Domain/Aggregates/Libraries/Services/ILibraryService.cs
+++ src/Service.Dashboard/src/Domain/Aggregates/Libraries/Services/ILibraryService.cs
@@ -7,10 +7,10 @@ namespace Giantnodes.Service.Dashboard.Domain.Aggregates.Libraries.Services;
 public interface ILibraryService : IDomainService
 {
     /// <summary>
-    /// Retrieves a read-only collection of <see cref="IFileSystemInfo" /> objects that represent the files and
-    /// subdirectories inside the specified library.
+    /// Retrieves a read-only collection of <see cref="IFileSystemInfo" /> objects that represent the media files and
+    /// any sub-directories inside the specified library.
     /// </summary>
-    /// <param name="library">A library specifying the path on which to search the file system.</param>
+    /// <param name="library">A library that specifies the path on which to search the file system.</param>
     /// <returns>An read-only collection of the file system entries that are inside the specified library path.</returns>
     /// <exception cref="DirectoryNotFoundException">Thrown when the path is not a directory or cannot be found.</exception>
     IReadOnlyCollection<IFileSystemInfo> GetFileSystemInfos(Library library);
--- src/Service.Dashboard/src/Domain/Aggregates/Libraries/Services/Impl/LibraryService.cs
+++ src/Service.Dashboard/src/Domain/Aggregates/Libraries/Services/Impl/LibraryService.cs
@@ -1,5 +1,8 @@
 ﻿using System.IO.Abstractions;
+using Giantnodes.Infrastructure;
 using Giantnodes.Service.Dashboard.Domain.Aggregates.Libraries.Entities;
+using Giantnodes.Service.Dashboard.Domain.Enumerations;
+using Giantnodes.Service.Dashboard.Domain.Shared;
 
 namespace Giantnodes.Service.Dashboard.Domain.Aggregates.Libraries.Services.Impl;
 
@@ -19,8 +22,10 @@ public class LibraryService : ILibraryService
         if (!root.Exists)
             throw new DirectoryNotFoundException();
 
+        // collect all the directories and media files inside the root directory
         var entries = root
             .GetFileSystemInfos("*", SearchOption.AllDirectories)
+            .Where(x => x is IDirectoryInfo || Enumeration.TryParse<MediaFileExtension>(x.Extension) != null)
             .ToList();
 
         return entries.AsReadOnly();
