fcf5d63be418cb983043511767d8126c50c29657
Jordan Phillips <jordypee27@gmail.com>
2023-08-05 14:38:09 +1000

    tests(dashboard): fix unit tests failing on linux based operating systems

--- src/Service.Dashboard/tests/Giantnodes.Service.Dashboard.Domain.Tests/Aggregates/Services/LibraryServiceTests.cs
+++ src/Service.Dashboard/tests/Giantnodes.Service.Dashboard.Domain.Tests/Aggregates/Services/LibraryServiceTests.cs
@@ -10,13 +10,13 @@ namespace Giantnodes.Service.Dashboard.Domain.Tests.Aggregates.Services;
 public class LibraryServiceTests
 {
     private readonly MockFileSystem _fs = new MockFileSystem(new Dictionary<string, MockFileData> {
-        { @"C:\tv-shows\Silicon Valley", new MockDirectoryData() },
-        { @"C:\tv-shows\Silicon Valley\Season 1", new MockDirectoryData() },
-        { @"C:\tv-shows\Silicon Valley\Season 1\.DS_Store", new MockFileData(string.Empty) },
-        { @"C:\tv-shows\Silicon Valley\Season 1\poster.png", new MockFileData(string.Empty) },
-        { @"C:\tv-shows\Silicon Valley\Season 1\Silicon Valley - S01E01 - Minimum Viable Product.mp4", new MockFileData(string.Empty) },
-        { @"C:\tv-shows\Silicon Valley\Season 1\Silicon Valley - S01E02 - The Cap Table.mp4", new MockFileData(string.Empty) },
-        { @"C:\tv-shows\Silicon Valley\Season 1\Silicon Valley - S01E03 - Articles of Incorporation.mkv", new MockFileData(string.Empty) }
+        { MockUnixSupport.Path(@"C:\tv-shows\Silicon Valley"), new MockDirectoryData() },
+        { MockUnixSupport.Path(@"C:\tv-shows\Silicon Valley\Season 1"), new MockDirectoryData() },
+        { MockUnixSupport.Path(@"C:\tv-shows\Silicon Valley\Season 1\.DS_Store"), new MockFileData(string.Empty) },
+        { MockUnixSupport.Path(@"C:\tv-shows\Silicon Valley\Season 1\poster.png"), new MockFileData(string.Empty) },
+        { MockUnixSupport.Path(@"C:\tv-shows\Silicon Valley\Season 1\Silicon Valley - S01E01 - Minimum Viable Product.mp4"), new MockFileData(string.Empty) },
+        { MockUnixSupport.Path(@"C:\tv-shows\Silicon Valley\Season 1\Silicon Valley - S01E02 - The Cap Table.mp4"), new MockFileData(string.Empty) },
+        { MockUnixSupport.Path(@"C:\tv-shows\Silicon Valley\Season 1\Silicon Valley - S01E03 - Articles of Incorporation.mkv"), new MockFileData(string.Empty) }
     });
 
     [Fact]
@@ -46,7 +46,7 @@ public class LibraryServiceTests
         var infos = service.GetFileSystemInfos(library);
 
         // assert
-        var paths = infos.Select(x => MockUnixSupport.Path(x.FullName)).ToList();
+        var paths = infos.Select(x => x.FullName).ToList();
         Assert.Contains(MockUnixSupport.Path(@"C:\tv-shows\Silicon Valley\Season 1\Silicon Valley - S01E01 - Minimum Viable Product.mp4"), paths);
         Assert.Contains(MockUnixSupport.Path(@"C:\tv-shows\Silicon Valley\Season 1\Silicon Valley - S01E02 - The Cap Table.mp4"), paths);
         Assert.Contains(MockUnixSupport.Path(@"C:\tv-shows\Silicon Valley\Season 1\Silicon Valley - S01E03 - Articles of Incorporation.mkv"), paths);
@@ -68,7 +68,7 @@ public class LibraryServiceTests
         var infos = service.GetFileSystemInfos(library);
 
         // assert
-        var paths = infos.Select(x => MockUnixSupport.Path(x.FullName)).ToList();
+        var paths = infos.Select(x => x.FullName).ToList();
         Assert.Contains(MockUnixSupport.Path(@"C:\tv-shows\Silicon Valley\Season 1"), paths);
     }
 
