68fe3b8c3d4e90974d48b9278ef91dc376d51c57
Jordan Phillips <jordypee27@gmail.com>
2024-02-07 17:37:15 +1100

    fix(dashboard): redirect on library slug change

--- app/src/app/(libraries)/library/[slug]/settings/general/page.tsx
+++ app/src/app/(libraries)/library/[slug]/settings/general/page.tsx
@@ -1,18 +1,26 @@
 'use client'
 
-import type { LibraryUpdateRef } from '@/components/forms/library'
+import type { LibraryUpdatePayload, LibraryUpdateRef } from '@/components/forms/library'
 
 import { Button, Card, Typography } from '@giantnodes/react'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 import { useLibraryContext } from '@/app/(libraries)/library/[slug]/use-library.hook'
 import { LibraryUpdate } from '@/components/forms/library'
 
 const LibrarySettingsGeneralPage = () => {
+  const router = useRouter()
   const { library } = useLibraryContext()
 
   const ref = React.useRef<LibraryUpdateRef>(null)
   const [isLoading, setLoading] = React.useState<boolean>(false)
+
+  const onComplete = (payload: LibraryUpdatePayload) => {
+    if (library.slug !== payload.slug) {
+      router.push(`/library/${payload.slug}/settings/general`)
+    }
+  }
 
   return (
     <section className="max-w-5xl">
@@ -22,7 +30,7 @@ const LibrarySettingsGeneralPage = () => {
         </Card.Header>
 
         <Card.Body>
-          <LibraryUpdate ref={ref} library={library} onLoadingChange={setLoading} />
+          <LibraryUpdate ref={ref} library={library} onComplete={onComplete} onLoadingChange={setLoading} />
         </Card.Body>
 
         <Card.Footer className="flex items-center justify-end gap-2">
