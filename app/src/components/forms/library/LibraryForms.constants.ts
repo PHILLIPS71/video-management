6648556544fc9d1ac04e622e1de7f464c8c55e73
Jordan Phillips <jordypee27@gmail.com>
2024-03-19 18:28:33 +1100

    chore: update dependencies + linting rules

--- app/src/components/forms/library/LibraryForms.constants.ts
+++ app/src/components/forms/library/LibraryForms.constants.ts
@@ -1,6 +1,5 @@
 import * as z from 'zod'
 
-// eslint-disable-next-line import/prefer-default-export
 export const SlugTransform = z.string().transform((val) =>
   val
     ?.toLowerCase()
--- app/src/components/widgets/index.ts
+++ app/src/components/widgets/index.ts
@@ -1,2 +1,1 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as ResolutionWidget } from '@/components/widgets/resolution/ResolutionWidget'
