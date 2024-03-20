f53ac42ee2075b3db12131827151b20b1ad27a39
Jordan Phillips <jordypee27@gmail.com>
2024-03-20 19:51:24 +1100

    docs: add comments into guard clauses

--- src/Infrastructure/src/Infrastructure/Guards/GuardAgainstNotFoundExtensions.cs
+++ src/Infrastructure/src/Infrastructure/Guards/GuardAgainstNotFoundExtensions.cs
@@ -1,10 +1,19 @@
-using System.Runtime.CompilerServices;
 using Giantnodes.Infrastructure;
 
 namespace Ardalis.GuardClauses;
 
 public static class GuardAgainstNotFoundExtensions
 {
+    /// <summary>
+    /// Throws a <see cref="NotFoundException"/> if the specified enumeration value is not found in the given collection.
+    /// </summary>
+    /// <typeparam name="T">The enumeration type.</typeparam>
+    /// <param name="guard">The guard clause instance.</param>
+    /// <param name="collection">The collection to search for the enumeration value.</param>
+    /// <param name="input">The enumeration value to check for.</param>
+    /// <returns>The input value if it is found in the collection.</returns>
+    /// <exception cref="NotFoundException">Thrown if the input value is not found in the collection.</exception>
+    /// <exception cref="ArgumentNullException">Thrown if the input value is null.</exception>
     public static T NotFound<T>(this IGuardClause guard, IEnumerable<T> collection, T input)
         where T : Enumeration
     {
