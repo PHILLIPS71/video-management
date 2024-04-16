80c7613cf8aaeefc7b91dc8f34b37d7aa80cf451
Jordan Phillips <jordypee27@gmail.com>
2024-04-16 20:47:43 +1000

    build: set next revalidate to 0 to prevent build time fetching

--- app/src/libraries/relay/network.ts
+++ app/src/libraries/relay/network.ts
@@ -19,6 +19,9 @@ export const execute = async (parameters: RequestParameters, variables: Variable
   const response = await fetch(HTTP_ENDPOINT, {
     method: 'POST',
     cache: 'no-store',
+    next: {
+      revalidate: 0,
+    },
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
