f1a010e4df3280ca7e314854b229a0475e888664
Jordan Phillips <jordypee27@gmail.com>
2024-06-02 12:39:53 +1000

    chore: docker setup (#59)
    * fix: allow all origins in cors by default
    
    * chore: add Dockerfiles to each service
    
    * fix: ffmpeg executable directory not creating when not found
    
    * fix: set nextjs environment variables to be evaluated at run-time
    
    * chore: add docker-compose to build images locally
    
    * fix: @giantnodes/theme tailwind styling not being built

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -3,7 +3,11 @@ import ReactAriaComponents from 'tailwindcss-react-aria-components'
 import { giantnodes } from '@giantnodes/theme'
 
 const config: Config = {
-  content: ['./src/**/*.{ts,tsx}', './node_modules/@giantnodes/theme/dist/**/*.{js,cjs}'],
+  content: [
+    './src/**/*.{ts,tsx}',
+    './node_modules/@giantnodes/theme/dist/**/*.{js,mjs,cjs}',
+    './node_modules/@giantnodes/react/dist/**/*.{js,mjs,cjs}',
+  ],
   plugins: [giantnodes(), ReactAriaComponents()],
   darkMode: 'class',
 }
